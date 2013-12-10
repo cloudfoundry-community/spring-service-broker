6d26995ad49c2dc39a39c6792916cd32399fe3b0
Steve Greenberg <spgreenberg@gmail.com>
2013-12-10 09:55:44 -0700

    Log general exceptions

--- src/main/java/com/pivotal/cf/broker/controller/BaseController.java
+++ src/main/java/com/pivotal/cf/broker/controller/BaseController.java
@@ -2,6 +2,8 @@ package com.pivotal.cf.broker.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -15,6 +17,8 @@ import com.pivotal.cf.broker.model.ErrorMessage;
 
 public abstract class BaseController {
 
+	private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	@ResponseBody
 	public ResponseEntity<ErrorMessage> handleException(
@@ -41,6 +45,7 @@ public abstract class BaseController {
 	public ResponseEntity<ErrorMessage> handleException(
 			Exception ex, 
 			HttpServletResponse response) {
+		logger.warn("Exception", ex);
 	    return getErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
